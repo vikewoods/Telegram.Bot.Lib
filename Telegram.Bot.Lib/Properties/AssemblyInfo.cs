fba371b3d37f5aa1340143b4b53c08a632d829c5
Vik Ewoods <vik.ewoods@gmail.com>
2015-11-19 21:18:33 +0200

    Initial commit

---
+++ Telegram.Bot.Lib/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Telegram.Bot.Lib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Telegram.Bot.Lib")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("50e5c4c9-385b-4dd1-863c-d0e6c18efe78")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("Telegram.Bot.Lib.Tests")]
+[assembly: InternalsVisibleTo("Telegram.Bot.Lib.Explorables")]
+
---
+++ Telegram.Bot.Lib/TelegramBot.cs
@@ -0,0 +1,64 @@
+﻿// Copyright(C) 2015 by Vik Ewoods <vik.ewoods@gmail.com>
+// 
+// This file is part of Telegram.Bot.Lib.
+// 
+// Project:  Telegram.Bot.Lib
+// File:        TelegramBot.cs
+// User:      vikew
+// Date:      20:05 19/11/2015
+// 
+// Telegram.Bot.Lib is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Telegram.Bot.Lib is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Telegram.Bot.Lib. If not, see <http://www.gnu.org/licenses/>.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace Telegram.Bot.Lib
+{
+    public class TelegramBot : ITelegramBot
+    {
+        private readonly string _token;
+        private const string BaseApiUrl = "https://api.telegram.org/";
+        private const string ApiUrl = "https://api.telegram.org/bot{0}/{1}";
+        private readonly RestClient _restClient;
+
+
+        /// <exception cref="ArgumentNullException"><paramref name="token" /> is <see langword="null" />.</exception>
+        public TelegramBot(string token)
+        {
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                throw new ArgumentNullException(nameof(token), "Token can't be blank!");
+            }
+
+            _token = token;
+            _restClient = new RestClient(BaseApiUrl);
+        }
+
+        private string BuildRequest(string method)
+        {
+            return $"bot{_token}/{method}";
+        }
+
+        public Task GetMe()
+        {
+            RestRequest restRequest = new RestRequest(BuildRequest("getMe"), Method.GET);
+            return null;
+        }
+    }
+}
